93290d13b79f3e0a91d90f373a69f74e31f093aa
Divine Bayuo <divinewb123@gmail.com>
2023-07-22 20:01:52 +0000

    0. Betty would be proud

---
+++ mul.c
@@ -0,0 +1,13 @@
+#include "shell_header.h"
+
+/**
+ * mul - function that multiplies two ints
+ * @a: 1st factor
+ * @b: 2nd factor
+ * Return: returns product
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ shell_header.h
@@ -0,0 +1,7 @@
+#ifndef SHELL_HEADER_H
+#define SHELL_HEADER_H
+
+int mul(int, int);
+
+#endif /* SHELL_HEADER_H *?
+
