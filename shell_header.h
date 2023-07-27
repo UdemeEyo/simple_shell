5e56113fc5e641077df0ac4d72fbcf4c9809ed50
UdemeEyo <udemeeyo2013@gmail.com>
2023-07-27 03:16:54 -0700

    DU

--- shell_header.h
+++ shell_header.h
@@ -15,4 +15,26 @@ int shell_prompt(void);
 void command_parser(char *str, char **cmd_arg);
 void command_exec(char **cmd_arg);
 
+/*builtins*/
+void all_cmd(void);
+void alias_cmd(void);
+void cd_cmd(void);
+void exit_cmd(void);
+void help_cmd(void);
+
+void help_env(void);
+void help_setenv(void);
+void help_unsetenv(void);
+void help_history(void);
+
+/*env_builtins*/
+int _setenv(char **args, char __attribute__((__unused__)) **front);
+int _unsetenv(char **args, char __attribute__((__unused__)) **front);
+int _env(char **args, char __attribute__((__unused__)) **front);
+
+/*ex_builtins*/
+int handle_alias(char **args, char __attribute__((__unused__)) **front);
+void set_alias_value(char *alias_name, char *alias_value);
+void print_aliases(alias_t *alias_list);
+
 #endif /* SHELL_HEADER_H */
